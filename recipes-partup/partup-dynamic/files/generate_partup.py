6e33ed36cfd2d0522337c76237423b851e810f89
Tony Kurz <tony.kurz@ml-pa.com>
2026-04-07 12:27:56 +0200

    feat: adding first lliot device

--- recipes-partup/partup-dynamic/files/generate_partup.py
+++ recipes-partup/partup-dynamic/files/generate_partup.py
@@ -1,41 +1,43 @@
 #!/usr/bin/env python3
 import sys
+import os
 
 BOARD = sys.argv[1]
-total_mb = int(sys.argv[2])
+TOTAL_MB = int(sys.argv[2])
+WORKDIR = os.getenv("WORKDIR")
+# Fixe Partitionen (MB)
+BOOT_SIZE = 128
+CONFIG_SIZE = 55
+SNAP_SIZE = 500
 
-# Fixe Partitionen
-boot_size = 130
-config_size = 60
-snap_size = 600
-
-# Rootfs A/B proportional berechnen
-rootfs_total = total_mb - (boot_size*2 + config_size + snap_size)
+# Rootfs A/B proportional
+rootfs_total = TOTAL_MB - (BOOT_SIZE*2 + CONFIG_SIZE + SNAP_SIZE)
 rootfs_size = rootfs_total // 2
 
-# Startadressen in MB
+# Startadressen
 boot0_start = 0
-boot1_start = boot0_start + boot_size
-config_start = boot1_start + boot_size
-rootfsA_start = config_start + config_size
+boot1_start = boot0_start + BOOT_SIZE
+config_start = boot1_start + BOOT_SIZE
+rootfsA_start = config_start + CONFIG_SIZE
 rootfsB_start = rootfsA_start + rootfs_size
 snap_start = rootfsB_start
 
-# XML erstellen
+# XML Partup erzeugen
 xml = f"""<?xml version="1.0"?>
 <partitions>
-    <partition name="boot0" start="{boot0_start}MB" size="{boot_size}MB" fstype="vfat"/>
-    <partition name="boot1" start="{boot1_start}MB" size="{boot_size}MB" fstype="vfat"/>
-    <partition name="config" start="{config_start}MB" size="{config_size}MB" fstype="ext4"/>
-    <partition name="rootfs_ext" type="extended" start="{rootfsA_start}MB" size="{rootfs_size*2 + snap_size}MB">
+    <partition name="boot0" start="{boot0_start}MB" size="{BOOT_SIZE}MB" fstype="vfat"/>
+    <partition name="boot1" start="{boot1_start}MB" size="{BOOT_SIZE}MB" fstype="vfat"/>
+    <partition name="config" start="{config_start}MB" size="{CONFIG_SIZE}MB" fstype="ext4"/>
+    <partition name="rootfs_ext" type="extended" start="{rootfsA_start}MB" size="{rootfs_size*2 + SNAP_SIZE}MB">
         <logical name="rootfsA" start="{rootfsA_start}MB" size="{rootfs_size}MB" fstype="ext4"/>
         <logical name="rootfsB" start="{rootfsB_start}MB" size="{rootfs_size}MB" fstype="ext4"/>
-        <logical name="snapdata" start="{snap_start}MB" size="{snap_size}MB" fstype="ext4"/>
+        <logical name="snapdata" start="{snap_start}MB" size="{SNAP_SIZE}MB" fstype="ext4"/>
     </partition>
 </partitions>
 """
 
-with open(f"{BOARD}-partup.xml", "w") as f:
+out_file = f"${WORKDIR}/{BOARD}.partup"
+with open(out_file, "w") as f:
     f.write(xml)
 
-print(f"{BOARD}-partup.xml erzeugt!")
+print(f"{out_file} erzeugt! Größe={TOTAL_MB}MB, RootfsA/B={rootfs_size}MB")
--- recipes-partup/partup-dynamic/partup-dynamic_0.1.bb
+++ recipes-partup/partup-dynamic/partup-dynamic_0.1.bb
@@ -1,8 +1,25 @@
-FILESEXTRAPATHS:prepend := "${THISDIR}/files:"
-LICENSE = "CLOSED"
+DESCRIPTION = "Generate Partup XML only (image packaging, no flash)"
+LICENSE = "MIT"
 
-do_configure() {
-    python3 ${THISDIR}/files/generate-partup.py ${BOARDNAME} ${EMMC_SIZE_MB}
+inherit python3native
+
+# Arbeitsverzeichnis
+WORKDIR = "${WORKDIR}"
+
+# Parameter setzen
+BOARDNAME ?= "phyboard-segin-imx93-2"
+EMMC_SIZE_MB ?= "7280"
+
+# Python Script aus Layer Files
+SRC_URI = "file://generate-partup.py"
+
+do_generate() {
+    import os
+    import subprocess
+
+    script = os.path.join(d.getVar('THISDIR', True), 'files', 'generate-partup.py')
+    subprocess.run(['python3', script, d.getVar('BOARDNAME', True), d.getVar('EMMC_SIZE_MB', True)], check=True)
 }
 
-SRC_URI += "file://${BOARDNAME}-partup.xml"
+# Nur generate-Task, kein flashen
+addtask generate after do_configure before do_build
